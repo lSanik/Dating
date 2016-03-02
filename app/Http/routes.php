474ce763986d7d83240913a0e25b91ff570fe932
Oleh <go_horizon@hotmail.com>
2016-03-02 17:42:06 +0200

    Views & DB

--- app/Http/routes.php
+++ app/Http/routes.php
@@ -111,7 +111,7 @@ Route::group([  'prefix' => 'admin',
         // @todo Create gifts routs
         // @todo Make gifts Controllers
         // @todo Make gifts DB
-    
+
     /** End gifts */
 
     /** Old porofile */
